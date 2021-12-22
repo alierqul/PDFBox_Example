1ea36209cd903f1863de5c2bbfba7ee33f83add0
Ali ERGÜL <alierqul@hotmail.com>
2021-12-22 12:36:25 +0300

    IText PDF creator Example

--- src/main/java/com/aliergul/itext/ITextPDFCreation.java
+++ src/main/java/com/aliergul/itext/ITextPDFCreation.java
@@ -1,5 +1,56 @@
 package com.aliergul.itext;
 
-public class ITextPDFCreation {
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import com.aliergul.pdfbox.App;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
+public class ITextPDFCreation {
+  private static final String PDF_FILE_PATH = "D:\\Eclipse2021\\ornek2.pdf";
+  private static final String PDF_IMAGE_PATH =
+      App.class.getResource("../../../email.png").toString().substring(6);// "file:\" kesmek için 6
+                                                                          // yazdım.
+
+  public static void main(String[] args) {
+
+    try {
+      createPDF();
+    } catch (IOException | DocumentException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  private static void createPDF() throws FileNotFoundException, IOException, DocumentException {
+    Document pdf = new Document(PageSize.A4, 20, 20, 20, 20);
+    System.out.println("PDF created");
+
+    try (FileOutputStream fos = new FileOutputStream(PDF_FILE_PATH)) { // içerik doldurmaca burada
+                                                                       // baþlýyor
+      PdfWriter writer = PdfWriter.getInstance(pdf, fos);
+      // writer.setEncryption("gizli".getBytes(), "mizli".getBytes(), PdfWriter.ALLOW_PRINTING,
+      // PdfWriter.ENCRYPTION_AES_128);
+
+      pdf.open();
+      pdf.add(new Paragraph("bir gün okula giderken..."));
+      pdf.add(new Paragraph("her şeye dikkat ederken..."));
+      pdf.add(new Paragraph("bir kız çıktı karşýma..."));
+      pdf.add(Image.getInstance(PDF_IMAGE_PATH));
+
+      pdf.addAuthor("Ali ERGÜL");
+      pdf.addCreationDate();
+      pdf.addCreator("Automated creation");
+      pdf.addTitle("iText ile PDF dosyası oluşturma");
+      pdf.addKeywords("pdf; java; bilgeadam;");
+      pdf.close();
+      writer.close();
+    }
+    System.out.println("PDF content written");
+  }
 }
